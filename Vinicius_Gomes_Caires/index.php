a3cce9bdbe432a9e7aa92c8565d56da9f2a4d2e9
Vinicius Gomes Caires <134635030+ViniciusGomesCaires@users.noreply.github.com>
2025-04-27 19:26:07 -0300

    Add files via upload

---
+++ Vinicius_Gomes_Caires/index.php
@@ -0,0 +1,50 @@
+<?php
+    require_once("db.php");
+    require_once("globals.php");
+?>
+<?php
+    require_once("templates/header.php");
+    ?>
+    <div id="main-container" class="container-fluid">
+    <h1>Conteúdo</h1>
+    </div>
+    <?php
+    require_once("templates/footer.php");
+    ?>
+    <!DOCTYPE html>
+    <html lang="pt_BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>DocumentFilmes</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+    <body>
+        <footer>
+        <footer id="footer">
+        <div id="social-container">
+        <ul>
+        <li>
+        <a href="#"><i class="fab fa-facebook-square"></i></a>
+        </li>
+        <li>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+        </li>
+        </ul>
+        </div>
+        <div id="footer-links-container">
+        <ul>
+        <li><a href="#">Adicionar filme</a></li>
+        <li><a href="#">Adicionar crítica</a></li>
+        <li><a href="#">Entrar / Registrar</a></li>
+        </ul>
+        </div>
+        <p>&copy; 2025 Vinicius Gomes Caires</p>
+        </footer>
+        </footer>
+    </body>
+    </html>
